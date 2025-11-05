5ac8ddbc11e35a6e06869ded9cbad33c9696683b
Jonathan Krall <jonathan.krall@lexicon.se>
2025-11-05 12:01:17 +0100

    Robot done -> Lunchbreak

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Personalregister
+﻿ 
+namespace Personalregister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            Robot robot = new Robot("R2D2", 67,24500);
+            string robotName = robot.GetName();
+            Console.WriteLine(robotName);
+            robot.SetName("Test");
+            robot.Age = -1;
+            Console.WriteLine(robot.GetName());
+            Console.WriteLine("Test");
+            Console.WriteLine(robot.Salary);
+            //robot.Salary = 25000;
+            robot.SetSalary(25000);
+
+
         }
     }
 }
---
+++ Personalregister/Robot.cs
@@ -0,0 +1,60 @@
+﻿namespace Personalregister
+{
+    internal class Robot
+    {
+        private string _name;
+
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+
+            set
+            {
+                if (value < 0)
+                    _age = 0;
+                else
+                    _age = value;
+            }
+        }
+
+        //public int Age { get; set; }
+
+        public int Salary { get;}
+
+        public void SetSalary(int salary)
+        {
+            //Salary = salary; 
+        }
+
+
+        public Robot(string name, int age, int salary)
+        {
+            _name = name;
+            _age = age;
+            Salary = salary; 
+        }
+        //public Robot(string name)
+        //{
+
+        //}
+        //public Robot()
+        //{
+
+        //}
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            //Validate
+            _name = name;
+        }
+
+
+    }
+}
