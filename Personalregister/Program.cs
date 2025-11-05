69a0b3118b7ee93b5e9cdd197ee480b0a7d91308
Jonathan Krall <jonathan.krall@lexicon.se>
2025-11-05 14:03:11 +0100

    Added SeedData

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,17 +1,27 @@
 ﻿
 
+
 namespace Personalregister
 {
     internal class Program
     {
+        private static Payroll payroll = new Payroll(); 
         static void Main(string[] args)
         {
             Payroll payroll = new Payroll(); 
-            //Employee emp = new Employee("Kalle",25000);
+            //Employee emp = new Employee("Kalle",25000);         
 
-            payroll.AddEmployee("Nisse", 30000);
+            SeedData();
+            Console.ReadKey(); 
+        }
 
-            
+        private static void SeedData()
+        {
+            payroll.AddEmployee("Nisse", 30000);            
+            payroll.AddEmployee("Anna", 35000);            
+            payroll.AddEmployee("Kalle", 40000);            
+            payroll.AddEmployee("Stina", 45000);            
+            payroll.AddEmployee("Sven", 50000);            
         }
     }
 }
