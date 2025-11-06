dc4eb37ada1855db4919696677356e1986d055db
Jonathan Krall <jonathan.krall@lexicon.se>
2025-11-06 15:12:34 +0100

    Moved functionality from Program to Main

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -6,65 +6,16 @@ namespace Personalregister
     internal class Program
     {
         private static Payroll _payroll = new Payroll();
-        private static IUI _ui = new ConsoleUI(); 
+        private static IUI _ui = new ConsoleUI();
         static void Main(string[] args)
         {
+            Payroll payroll = new Payroll();
+            IUI ui = new ConsoleUI(); 
 
-            SeedData();
-            do
-            {
-                ShowMainMenu();
-                string input = _ui.GetInput();
-
-                switch (input)
-                {
-                    case MenuHelpers.Add:
-                        AddEmployee();
-                        break;
-                    case MenuHelpers.Print:
-                        PrintEmployees();
-                        break;
-                    case MenuHelpers.Quit:
-                        Environment.Exit(0); 
-                        break;
-                    default:
-                        break;
-                }
-            }
-            while (true);
+            Main main = new Main(ui, payroll);
+            main.Run();
         }
 
-        private static void AddEmployee()
-        {
-            string name = Util.AskForString("Name");
-            uint salary = Util.AskForUInt("Salary");
-            _payroll.AddEmployee(name, salary); 
-
-        }
-
-        private static void PrintEmployees()
-        {
-            IEnumerable<Employee> employees = _payroll.GetEmployees();
-
-            //employees[0].Name = "Sten-Ove"; 
-            foreach (Employee employee in employees)
-            {
-               _ui.Print(employee.Print());
-            }
-        }
-
-        private static void ShowMainMenu()
-        {
-            _ui.Print($"{MenuHelpers.Add}.Add {Environment.NewLine}{MenuHelpers.Print}.Print {Environment.NewLine}{MenuHelpers.Quit}.Quit");
-        }
-
-        private static void SeedData()
-        {
-            _payroll.AddEmployee("Örjan", 30000);
-            _payroll.AddEmployee("Anna", 35000);
-            _payroll.AddEmployee("Kalle", 40000);
-            _payroll.AddEmployee("Åsa", 45000);
-            _payroll.AddEmployee("Åke", 50000);
-        }
+        
     }
 }
