c67ca38ad7ab17dd5d2d279425dc46a01104b076
Jonathan Krall <jonathan.krall@lexicon.se>
2025-11-06 14:05:09 +0100

    Added SalaryLevel Enum

--- Personalregister/Employee.cs
+++ Personalregister/Employee.cs
@@ -6,6 +6,21 @@
         public string Name { get; set; }
         public uint Salary { get; set; }
 
+        public SalaryLevel SalaryLevel => Salary < 25000 ? SalaryLevel.Junior : SalaryLevel.Senior;
+        //{
+        //    get { 
+        //        if(Salary < 25000)
+        //        {
+        //            return SalaryLevel.Junior;
+        //        }
+        //        else
+        //        {
+        //            return SalaryLevel.Senior; 
+        //        }
+        //    }
+        
+        //}
+
         public Employee(string name, uint salary)
         {
             this.Name = name;
